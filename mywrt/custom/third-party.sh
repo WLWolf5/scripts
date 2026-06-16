1cb08ef9a8675afa1e62a058291d50100ec9f6d7
WLWolf5 <43511929+WLWolf5@users.noreply.github.com>
2026-06-16 22:02:06 +0800

    Update third-party.sh

--- mywrt/custom/third-party.sh
+++ mywrt/custom/third-party.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git clone --depth 1 https://github.com/muink/luci-app-natmapt ../luci
+git clone --depth 1 https://github.com/muink/openwrt-natmapt ../luci
+git clone --depth 1 https://github.com/muink/openwrt-stuntman ../luci
+
 cd ../tools
 ./svn.sh https://github.com/EasyTier/luci-app-easytier main luci-app-easytier
 ./svn.sh https://github.com/sundaqiang/openwrt-packages master luci-app-wolplus
