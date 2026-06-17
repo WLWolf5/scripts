361d6e65b8f8d61571108469e838a698cf5b46a0
WLWolf5 <43511929+WLWolf5@users.noreply.github.com>
2026-06-18 02:07:29 +0800

    Update patch.sh

--- mywrt/custom/patch.sh
+++ mywrt/custom/patch.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sed -i 's/util.pcdata/xml.pcdata/g' ../luci/luci-app-easytier/luasrc/model/cbi/easytier.lua
+
+sed -i 's/192.168.6.1/192.168.1.1/g' ${REPO_DIR}/package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/Xiaomi-WR30U/g' ${REPO_DIR}/package/base-files/files/bin/config_generate
