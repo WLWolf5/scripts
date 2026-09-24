56d2542474ad22c540bbeb45c8a59cd22e6a4e00
WLWolf5 <782558181@qq.com>
2026-09-24 05:52:58 +0000

    update

--- mywrt/custom/third-party.sh
+++ mywrt/custom/third-party.sh
@@ -4,6 +4,6 @@ git clone --depth 1 https://github.com/muink/luci-app-natmapt ../luci/luci-app-n
 git clone --depth 1 https://github.com/muink/openwrt-natmapt ../luci/natmapt
 git clone --depth 1 https://github.com/muink/openwrt-stuntman ../luci/stuntman
 
-cd ../tools
+cd ../../utils
 ./svn.sh https://github.com/EasyTier/luci-app-easytier main luci-app-easytier
 ./svn.sh https://github.com/sundaqiang/openwrt-packages master luci-app-wolplus
